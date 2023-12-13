6b7364cfaf2837012fbd2a2001665f8920a1e775
Ruhiel <ruhiel.murrue@gmail.com>
2023-12-13 21:47:12 +0900

    初回コミット

---
+++ UniDic2AIVoiceDic/Program.cs
@@ -0,0 +1,88 @@
+﻿using Csv;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UniDic2AIVoiceDic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var csv = File.ReadAllText(@"E:\unidic-csj-3.1.1\lex_3_1.csv");
+            var options = new CsvOptions()
+            {
+                HeaderMode = HeaderMode.HeaderAbsent,
+            };
+            var hash = new HashSet<string>();
+
+            using (var sw = new StreamWriter(@"result.txt", false, Encoding.UTF8))
+            using (var sw2 = new StreamWriter(@"kanji.txt", false, Encoding.Default))
+            {
+                var lines = CsvReader.ReadFromText(csv, options);
+                foreach (var line in lines)
+                {
+                    var go = line[0];
+                    var type = string.Join("-", line[4], line[5], line[6]);
+                    var yomi = line[13];
+                    var src = line[10];
+                    if (type.Contains("助動詞") || type.Contains("数詞") || type.Contains("助詞") || type.Contains("連体詞") || type.Contains("非自立可能") || type.Contains("記号") || type.Contains("感動詞") || type.Contains("空白") || type.Contains("副詞") || type.Contains("一般") || type.Contains("人名") || type.Contains("形状詞") || type.Contains("接頭辞") || type.Contains("代名詞"))
+                    {
+                        continue;
+                    }
+                    if (Regex.IsMatch(go, "[=/㌁-㍖～゛~〜]") || Regex.IsMatch(go, "^[a-zA-Z0-9Ａ-Ｚａ-ｚ]*$"))
+                    {
+                        continue;
+                    }
+                    if (Regex.IsMatch(go, @"^\p{IsCJKUnifiedIdeographs}+$"))
+                    {
+                        if (StringUtil.ConvertEncoding(go, Encoding.GetEncoding("Shift_JIS")))
+                        {
+                            sw2.WriteLine($"{go} {yomi}");
+                            continue;
+                        }
+                    }
+
+                    if (type == "名詞-普通名詞-サ変可能")
+                    {
+                        type = "名詞-サ変接続";
+                    }
+
+                    if (type == "接尾辞-名詞的-サ変可能")
+                    {
+                        type = "名詞-サ変接続";
+                    }
+                    else if (type == "名詞-固有名詞-地名")
+                    {
+                        type = "名詞-固有名詞-地域-一般";
+                    }
+                    else if (type == "名詞-普通名詞-助数詞可能")
+                    {
+                        type = "名詞-一般";
+                    }
+                    else if (type == "接続詞-*-*")
+                    {
+                        type = "名詞-一般";
+                    }
+                    else if (type == "接尾辞-動詞的-*")
+                    {
+                        type = "名詞-一般";
+                    }
+                    else if (type == "接尾辞-形容詞的-*")
+                    {
+                        type = "名詞-一般";
+                    }
+                    if (!hash.Contains(go.Han2Zen()))
+                    {
+                        sw.WriteLine(string.Join(";", type, go, "2000", yomi, $"0-{Math.Min(src.Length, yomi.Length)}:*"));
+                        hash.Add(go);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UniDic2AIVoiceDic/StringUtil.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniDic2AIVoiceDic
+{
+    static class StringUtil
+    {
+        public static bool ConvertEncoding(string src, Encoding destEnc)
+        {
+            byte[] src_temp = Encoding.UTF8.GetBytes(src);
+            byte[] dest_temp = Encoding.Convert(Encoding.UTF8, destEnc, src_temp);
+            string ret = destEnc.GetString(dest_temp);
+            return ret.Contains("?");
+        }
+
+        // WIN32API:LCMapStringWの宣言
+        [DllImport("kernel32.dll")]
+        static extern private int LCMapStringW(int Locale, uint dwMapFlags,
+            [MarshalAs(UnmanagedType.LPWStr)] string lpSrcStr, int cchSrc,
+            [MarshalAs(UnmanagedType.LPWStr)] string lpDestStr, int cchDest);
+
+        public enum dwMapFlags : uint
+        {
+            NORM_IGNORECASE = 0x00000001,           //大文字と小文字を区別しません。
+            NORM_IGNORENONSPACE = 0x00000002,       //送りなし文字を無視します。このフラグをセットすると、日本語アクセント文字も削除されます。
+            NORM_IGNORESYMBOLS = 0x00000004,        //記号を無視します。
+            LCMAP_LOWERCASE = 0x00000100,           //小文字を使います。
+            LCMAP_UPPERCASE = 0x00000200,           //大文字を使います。
+            LCMAP_SORTKEY = 0x00000400,             //正規化されたワイド文字並び替えキーを作成します。
+            LCMAP_BYTEREV = 0x00000800,             //Windows NT のみ : バイト順序を反転します。たとえば 0x3450 と 0x4822 を渡すと、結果は 0x5034 と 0x2248 になります。
+            SORT_STRINGSORT = 0x00001000,           //区切り記号を記号と同じものとして扱います。
+            NORM_IGNOREKANATYPE = 0x00010000,       //ひらがなとカタカナを区別しません。ひらがなとカタカナを同じと見なします。
+            NORM_IGNOREWIDTH = 0x00020000,          //シングルバイト文字と、ダブルバイトの同じ文字とを区別しません。
+            LCMAP_HIRAGANA = 0x00100000,            //ひらがなにします。
+            LCMAP_KATAKANA = 0x00200000,            //カタカナにします。
+            LCMAP_HALFWIDTH = 0x00400000,           //半角文字にします（適用される場合）。
+            LCMAP_FULLWIDTH = 0x00800000,           //全角文字にします（適用される場合）。
+            LCMAP_LINGUISTIC_CASING = 0x01000000,   //大文字と小文字の区別に、ファイルシステムの規則（既定値）ではなく、言語上の規則を使います。LCMAP_LOWERCASE、または LCMAP_UPPERCASE とのみ組み合わせて使えます。
+            LCMAP_SIMPLIFIED_CHINESE = 0x02000000,  //中国語の簡体字を繁体字にマップします。
+            LCMAP_TRADITIONAL_CHINESE = 0x04000000, //中国語の繁体字を簡体字にマップします。
+        }
+
+        // 文字列を変換する
+        public static string StringConvert(this string str, dwMapFlags flags)
+        {
+            var ci = System.Globalization.CultureInfo.CurrentCulture;
+            // すべての文字が濁音or半濁音かもしれないので２倍化
+            string result = new string(' ', str.Length * 2);
+            LCMapStringW(ci.LCID, (uint)flags, str, str.Length, result, result.Length);
+            return result.TrimEnd();
+        }
+
+        // 半角を全角に変換
+        public static string Han2Zen(this string str)
+        {
+            return StringConvert(str, dwMapFlags.LCMAP_FULLWIDTH);
+        }
+
+        // 全角を半角に変換
+        public static string Zen2Han(this string str)
+        {
+            return StringConvert(str, dwMapFlags.LCMAP_HALFWIDTH | dwMapFlags.LCMAP_KATAKANA);
+        }
+    }
+}
